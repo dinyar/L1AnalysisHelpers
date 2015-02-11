3cb7cc16738479e654968d1432c8581aca11b402
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-02-11 13:05:44 -0800

    Added comments on the interface, restructured to code, added axis labels etc.

--- CreateHistograms.py
+++ CreateHistograms.py
@@ -2,7 +2,14 @@
 
 from ROOT import *
 
-def generateEfficiencyHist(varList):
+## varlist entries:
+# 0: descriptive string used for caption and filename (what is plotted)
+# 1-3: binning
+# 4: variables to plot
+# 5: physical cuts on GMT muons
+# 6: physical cuts on reco (and GMT) muons
+# 7: list of 'cuts' for each component of stack (optional, used for subsystem separation)
+def generateEfficiencyHist(varList, dataset=""):
     gStyle.SetOptStat(0);
     legend = TLegend(0.70,0.5,0.95,0.8);
 
@@ -13,7 +20,7 @@ def generateEfficiencyHist(varList):
     stackTitle = "Efficiency vs. " + varList[0] + descrWspaces + varList[6][1]
 
     c1 = TCanvas('c1', canvasTitle, 200, 10, 700, 500)
-    histStack = THStack("histStack", stackTitle)
+    histStack = THStack("histStack", stackTitle + ";" + varList[0] + ";Efficiency")
     tmpHist = TH1D("tmpHist", "", varList[1], varList[2], varList[3])
     ntuple.Project("tmpHist", varList[4], varList[6][0])
 
@@ -29,7 +36,9 @@ def generateEfficiencyHist(varList):
     else:
         filenameMod = "stack_"
 
-    filename = "plots/hist_eff_"+ filenameMod +varList[0] + descrWOspaces + varList[6][1]+".pdf"
+    if dataset != "":
+        dataset += "_"
+    filename = "plots/hist_eff_" + dataset + filenameMod + varList[0] + descrWOspaces + varList[6][1] + ".pdf"
 
     for cutString in cutStrings:
         efficiencyHist = TH1D("effHist", cutString[0], varList[1], varList[2], varList[3])
@@ -43,27 +52,73 @@ def generateEfficiencyHist(varList):
         legend.Draw("")
     c1.Update()
     c1.Print(filename, "pdf")
-    
-def generate2DEfficiencyHist(varList):
-    gStyle.SetOptStat(0)
-    c1 = TCanvas('c1', "Efficiency vs. " + varList[0] + " - " + varList[8][1] + ", " + varList[9][1], 200, 10, 700, 500)
-    tmpHist = TH2D("tmpHist", "", varList[1], varList[2], varList[3], varList[4], varList[5], varList[6])
-    efficiencyHist = TH1D("effHist", "Efficiency vs. " + varList[0] + " - " + varList[8][1] + ", " + varList[9][1], varList[1], varList[2], varList[3], varList[4], varList[5], varList[6])
-    ntuple.Project("tmpHist", varList[7], varList[9][0])
-    ntuple.Project("effHist", varList[7], varList[9][0] + " && " + varList[8][0])
-    efficiencyHist.Divide(tmpHist)
-    efficiencyHist.DrawCopy()
-    c1.Update()
-    c1.Print("plots/hist2D_eff_"+varList[0]+"_"+varList[8][1] + "_" + varList[9][1]+".pdf", "pdf")
 
-def generateRateHist(varList):
+## varlist entries:
+# 0: descriptive string used for caption and filename (what is plotted)
+# 1-3: binning
+# 4: variables to plot
+# 5: physical cuts
+# TODO: 7: list of 'cuts' for each component of stack (optional, used for subsystem separation)
+def generateRateHist(varList, dataset = ""):
     gStyle.SetOptStat(110011);
     c1 = TCanvas('c1', "Rate of " + varList[0] + " - " + varList[5][1], 200, 10, 700, 500)
     rateHist = TH1D("rateHist", "Rate of " + varList[0] + " - " + varList[5][1], varList[1], varList[2], varList[3])
     ntuple.Project("rateHist", varList[4], varList[5][0])
+    rateHist.GetXaxis().SetTitle(varList[0])
     rateHist.DrawCopy()
     c1.Update()
-    c1.Print("plots/hist_rate_"+varList[0]+"_"+varList[5][1]+".pdf", "pdf")
+    if dataset != "":
+        dataset += "_"
+    filename = "plots/hist_rate_" + dataset + varList[0] + "_" + varList[5][1] + ".pdf"
+    c1.Print(filename, "pdf")
+    
+## varlist entries:
+# 0: descriptive string used for caption and filename (what is plotted)
+# 1-3: Binning for first variable
+# 4-6: Binning for second variable
+# 7: Variables to plot (in the form x2:x1) (!)
+# 8: physical cuts on GMT muons
+# 9: physical cuts on reco (and GMT) muons
+def generate2DEfficiencyHist(varList, dataset = ""):
+    gStyle.SetOptStat(0)
+    descrWspaces = " - " + varList[8][1] + ", "
+    descrWOspaces = "_"+varList[8][1] + "_"
+    c1 = TCanvas('c1', "Efficiency vs. " + varList[0] + descrWspaces + varList[9][1], 200, 10, 700, 500)
+    tmpHist = TH2D("tmpHist", "", varList[1], varList[2], varList[3], varList[4], varList[5], varList[6])
+    efficiencyHist = TH2D("effHist", varList[0] + descrWspaces + varList[9][1], varList[1], varList[2], varList[3], varList[4], varList[5], varList[6])
+    ntuple.Project("tmpHist", varList[7], varList[9][0])
+    ntuple.Project("effHist", varList[7], varList[8][0] + " && " + varList[9][0])
+    efficiencyHist.Divide(tmpHist)
+    axLbl = varList[7].split(":")
+    efficiencyHist.GetXaxis().SetTitle(axLbl[1])
+    efficiencyHist.GetYaxis().SetTitle(axLbl[0])
+    efficiencyHist.DrawCopy("COLZ")
+    c1.Update()
+    if dataset != "":
+        dataset += "_"
+    filename = "plots/hist2D_eff_" + dataset + varList[0] + descrWOspaces + varList[9][1] + ".pdf"
+    c1.Print(filename, "pdf")
+
+## varlist entries:
+# 0: descriptive string used for caption and filename (what is plotted)
+# 1-3: Binning for first variable
+# 4-6: Binning for second variable
+# 7: Variables to plot (in the form x1:x2)
+# 8: physical cuts
+def generate2DRateHist(varList, dataset = ""):
+    gStyle.SetOptStat(110011);
+    c1 = TCanvas('c1', "Rate of " + varList[0] + " - " + varList[8][1], 200, 10, 700, 500)
+    rateHist = TH2D("rateHist", varList[0] + " - " + varList[8][1], varList[1], varList[2], varList[3], varList[4], varList[5], varList[6])
+    ntuple.Project("rateHist", varList[7], varList[8][0])
+    axLbl = varList[7].split(":")
+    rateHist.GetXaxis().SetTitle(axLbl[0])
+    rateHist.GetYaxis().SetTitle(axLbl[1])
+    rateHist.DrawCopy("COLZ")
+    c1.Update()
+    if dataset != "":
+        dataset += "_"
+    filename = "plots/hist2D_rate_" + dataset + varList[0] + "_" + varList[8][1] + ".pdf"
+    c1.Print(filename, "pdf")
 
 # etaScalePos= [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.75,1.8,1.85,1.9,1.95,2.,2.05,2.1,2.15,2.2,2.25,2.3,2.35,2.4];
 # etaScale= [None] * 63
